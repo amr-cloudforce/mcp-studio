0574deefda8daaf9150458e5b305f92182b841c7
Amr <amr.cloudforce@protonmail.com>
2025-06-02 10:32:56 +0200

    wip

---
+++ repomix.sh
@@ -0,0 +1,4 @@
+find . -name "*.js" -not -path "./node_modules/*" -not -path "./dist/*" -not -path "./test-results/*" -not -path "./playwright-report/*" -exec sh -c 'echo "\n=== $1 ==="; grep -E "^\s*(//|/\*|\*)" "$1" || echo "No comments found"' _ {} \; > repomix-output.txt
+
+#cd docs  && rm -f repomix-output.* && npx repomix --style plain  --compress --remove-comments   --remove-empty-lines    .  && mv repomix-output.* .. && cd ..
+
